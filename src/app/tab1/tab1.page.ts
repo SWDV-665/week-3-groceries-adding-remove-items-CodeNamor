075011d988f9a4173d3047076ebde7e910eabe1b
F Derek Roman <derek.roman81@gmail.com>
2019-04-06 10:27:13 -0500

    adding CRUD operations

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +8,7 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  constructor(public alertController: AlertController) {}
+  constructor(public alertController: AlertController, public toastCtrl: ToastController) {}
 
   title = "Grocery";
 
@@ -74,7 +75,62 @@ export class Tab1Page {
 
   async removeItem(item, index) {
     console.log("Removing Item - ", item, index);
+    const toast = await this.toastCtrl.create({
+      message: 'Removing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
 
     this.items.splice(index, 1)
+  }
+
+  async editItem(item, index) {
+    console.log("Edit Item - ", item, index);
+    const toast = await this.toastCtrl.create({
+      message: 'Editing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+    this.showEditItemPromt(item, index);''
+
+  }
+
+  async showEditItemPromt(item, index) {
+    const alert = await this.alertController.create({
+      header: 'Edit item',
+      message: "Please edit Item ...",
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Name',
+          value: item.name
+        },
+        {
+          name: 'quantity',
+          type: 'text',
+          placeholder: 'quantity',
+          value: item.quantity
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: item => {
+            console.log('Confirm Ok', item);
+            this.items[index] = item;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
